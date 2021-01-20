0127a1ecc39d28dda4e39f69554569daa3bb6136
JadivaM <jmont261@fiu.edu>
2021-01-19 20:19:42 -0500

    added update user inluding allowed updates logic to user controller file

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -39,3 +39,20 @@ exports.loginUser = async (req, res) => {
 };
 
 exports.getCurrentUser = async (req, res) => res.json(req.user);
+
+exports.updateCurrentUser = async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ['name', 'email', 'password'];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+  if (!isValidOperation)
+    return res.status(400).send({ error: 'Invalid updates' });
+  try {
+    updates.forEach((update) => (req.user[update] = req.body[update]));
+    await req.user.save();
+    res.json(req.user);
+  } catch (e) {
+    res.status(400).send({ error: e.toString() });
+  }
+};
