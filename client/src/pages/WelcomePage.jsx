ce5a5306776f16af8d0a4aba19e8bb590ea2d8f0
Jadivam <Jmont261@fiu.edu>
2021-03-05 20:26:15 -0500

    styled homepage and added react bootstrap buttons

--- client/src/pages/WelcomePage.jsx
+++ client/src/pages/WelcomePage.jsx
@@ -1,15 +1,43 @@
 import React from 'react';
 import NavigationBar from '../components/NavigationBar';
-// import computerImage from '../images/3d-computer-art.png';
+import { Button } from 'react-bootstrap';
 
 const WelcomePage = () => {
   return (
     <>
-      <div className="welcome-page-hero-image-container">
+      <div className="welcome-page-main-container">
         <NavigationBar />
-        {/* <img className="welcome-page-hero-image-container-image" src={computerImage} alt="laptop illustration" /> */}
         <div className="welcome-page-hero-image-container-text">
-          <h2></h2>
+          <h1 className="welcome-page-hero-image-text-title">
+            Made for Developers
+          </h1>
+          <h5 className="welcome-page-hero-image-text-title--caption">
+            Easily search, save, and follow Github users in one place
+          </h5>
+          <Button
+            variant="link"
+            style={{
+              color: 'white',
+              backgroundColor: '#0f3c49',
+              border: 'none',
+              width: 100,
+              marginRight: 40,
+              marginTop: 20
+            }}
+          >
+            Log in
+          </Button>
+          <Button
+            style={{
+              color: 'white',
+              backgroundColor: '#0f3c49',
+              border: 'none',
+              width: 100,
+              marginTop: 20
+            }}
+          >
+            Sign up
+          </Button>
         </div>
       </div>
     </>
