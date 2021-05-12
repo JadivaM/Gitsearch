9320e8aa0828d49574f4c6ef958f3fa3b0c427e9
Jadivam <Jmont261@fiu.edu>
2021-05-12 01:11:49 -0400

    style profile and saved github user cards

--- client/src/components/SavedGithubUsers.jsx
+++ client/src/components/SavedGithubUsers.jsx
@@ -23,41 +23,49 @@ const SavedGithubUsers = () => {
 
   return (
     <div className="githubUserData-cards">
-      {githubUserData?.map((githubUserData) => {
-        return (
-          <Card style={{ width: 310 }}>
-            <Card.Img
-              variant="top"
-              src={githubUserData.avatar_url}
-              style={{
-                height: 250,
-                width: 250,
-                display: 'flex',
-                justifyContent: 'center',
-                margin: 'auto',
-                paddingTop: 10
-              }}
-            />
-            <Card.Body>
-              <Card.Title style={{ textAlign: 'center' }}>
-                {githubUserData.name}
-              </Card.Title>
-              <Card.Subtitle
-                className="mb-2 text-muted"
-                style={{ textAlign: 'center' }}
-              >
-                {githubUserData.login}
-              </Card.Subtitle>
-              <Card.Text>Profile URL: {githubUserData.html_url}</Card.Text>
-              <Card.Text>Repos URL: {githubUserData.repos_url}</Card.Text>
-              <a href={githubUserData.html_url}>
-                <Button variant="outline-primary">View profile</Button>
-              </a>
-              <DeleteButton githubUserData={githubUserData} />
-            </Card.Body>
-          </Card>
-        );
-      })}
+      <h1 className="saved-users-header">Saved users</h1>
+      {!githubUserData || githubUserData.length == 0 ? (
+        <p className="no-saved-users-text">No saved users yet :(</p>
+      ) : (
+        githubUserData?.map((githubUserData) => {
+          return (
+            <Card id="saved-users-card" style={{ width: 310 }}>
+              <Card.Img
+                variant="top"
+                src={githubUserData.avatar_url}
+                id="saved-users-card-image"
+              />
+              <Card.Body>
+                <Card.Title
+                  id="saved-users-name"
+                  style={{ textAlign: 'center' }}
+                >
+                  {githubUserData.name}
+                </Card.Title>
+                <Card.Subtitle
+                  id="saved-users-username"
+                  className="mb-2 text-muted"
+                  style={{ textAlign: 'center' }}
+                >
+                  {githubUserData.login}
+                </Card.Subtitle>
+                <Card.Text id="saved-users-profile-url">
+                  Profile URL: {githubUserData.html_url}
+                </Card.Text>
+                <a href={githubUserData.html_url} target="_blank">
+                  <Button
+                    id="saved-users-profile-button"
+                    variant="outline-primary"
+                  >
+                    View profile
+                  </Button>
+                </a>
+                <DeleteButton githubUserData={githubUserData} />
+              </Card.Body>
+            </Card>
+          );
+        })
+      )}
     </div>
   );
 };
